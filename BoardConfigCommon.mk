15b8afe381a0b1bf0327227f5e1c0527d0fd3d85
Arjun-Ingole <arjuningole38@gmail.com>
2021-11-10 21:29:44 +0600

    miatoll: Add wifi hostapd and supplicant to a separate compatibility matrix
    Signed-off-by: Arjun-Ingole <arjuningole38@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -69,7 +69,8 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/device_framework_matrix.xml
+    $(COMMON_PATH)/device_framework_matrix.xml \
+    $(COMMON_PATH)/configs/wifi_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
