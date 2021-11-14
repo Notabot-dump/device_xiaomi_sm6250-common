a217efb013a932ec146c6baaf1a57b8fe22ff817
Arjun-Ingole <arjuningole38@gmail.com>
2021-11-14 19:27:51 +0530

    miatoll: Provide some missing bluetooth libraries
    Could not find 'vendor.qti.hardware.btconfigstore@2.0::IBTConfigStore/default' for ctl.interface_start from pid: 553 (/system/bin/hwservicemanager)
    
    And Similar for others
    
    Change-Id: I0b17fcd94d447d8c03961600ee4a8f57873d4f90
    Signed-off-by: Arjun-Ingole <arjuningole38@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -78,8 +78,14 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0 \
+    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1 \
+    android.hardware.bluetooth@1.1.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0 \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0 \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
