57941808e3e08feec012ac952e4613fb24e9b054
Arjun-Ingole <arjuningole38@gmail.com>
2021-11-16 10:40:35 +0530

    sm6250-common: Explicitly bluid QTI camera hal
    Signed-off-by: Arjun-Ingole <arjuningole38@gmail.com>

--- common.mk
+++ common.mk
@@ -104,6 +104,8 @@ PRODUCT_PACKAGES += \
     libgui_vendor
 
 PRODUCT_PACKAGES += \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
