4fb7b3a0ae8eb9b245007c7295078d905d30a4d7
Arjun-Ingole <arjuningole38@gmail.com>
2021-11-16 10:38:40 +0530

    sm6250-common: Build Missing bluetooth blobs Logs are complaining
    Signed-off-by: Arjun-Ingole <arjuningole38@gmail.com>

--- common.mk
+++ common.mk
@@ -81,9 +81,15 @@ PRODUCT_PACKAGES += \
     BluetoothQti
 
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0 \
+    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1 \
+    android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.btconfigstore@1.0 \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0 \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
